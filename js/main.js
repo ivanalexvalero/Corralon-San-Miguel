55fc64d115c272251d7593f7b6ce09dc02628c0b
ivanalexvalero <ivanvalero1985@gmail.com>
2020-08-28 16:42:43 -0300

    4

--- js/main.js
+++
@@ -1,7 +0,0 @@
-function ocultar(){
-    document.getElementById(mostrarocultar).style.display="none";
-}
-
-function mostrar(){
-    document.getElementById(mostrarocultar).style.display="block";
-}
