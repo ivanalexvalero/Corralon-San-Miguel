fb7c40c375822e26e15cb0e8699e7673d8777482
ivanalexvalero <ivanvalero1985@gmail.com>
2020-08-28 16:22:08 -0300

    1

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function ocultar(){
+    document.getElementById(mostrarocultar).style.display="none";
+}
+
+function mostrar(){
+    document.getElementById(mostrarocultar).style.display="block";
+}
